a1c079317d1fb0be6c3d7e58a5d655c4c56b98e7
99BILL\andy.an <andy.an@99bill.com>
2019-04-10 11:00:11 +0800

    base on mybatis-3.5.1

--- src/main/java/org/apache/ibatis/annotations/Options.java
+++ src/main/java/org/apache/ibatis/annotations/Options.java
@@ -1,28 +1,28 @@
 /**
- * Copyright 2009-2018 the original author or authors.
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *    Copyright 2009-2019 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
  */
 package org.apache.ibatis.annotations;
-
-import org.apache.ibatis.mapping.ResultSetType;
-import org.apache.ibatis.mapping.StatementType;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+
+import org.apache.ibatis.mapping.ResultSetType;
+import org.apache.ibatis.mapping.StatementType;
 
 /**
  * @author Clinton Begin
@@ -31,36 +31,36 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface Options {
-    boolean useCache() default true;
+  /**
+   * The options for the {@link Options#flushCache()}.
+   * The default is {@link FlushCachePolicy#DEFAULT}
+   */
+  enum FlushCachePolicy {
+    /** <code>false</code> for select statement; <code>true</code> for insert/update/delete statement. */
+    DEFAULT,
+    /** Flushes cache regardless of the statement type. */
+    TRUE,
+    /** Does not flush cache regardless of the statement type. */
+    FALSE
+  }
 
-    FlushCachePolicy flushCache() default FlushCachePolicy.DEFAULT;
+  boolean useCache() default true;
 
-    ResultSetType resultSetType() default ResultSetType.DEFAULT;
+  FlushCachePolicy flushCache() default FlushCachePolicy.DEFAULT;
 
-    StatementType statementType() default StatementType.PREPARED;
+  ResultSetType resultSetType() default ResultSetType.DEFAULT;
 
-    int fetchSize() default -1;
+  StatementType statementType() default StatementType.PREPARED;
 
-    int timeout() default -1;
+  int fetchSize() default -1;
 
-    boolean useGeneratedKeys() default false;
+  int timeout() default -1;
 
-    String keyProperty() default "";
+  boolean useGeneratedKeys() default false;
 
-    String keyColumn() default "";
+  String keyProperty() default "";
 
-    String resultSets() default "";
+  String keyColumn() default "";
 
-    /**
-     * The options for the {@link Options#flushCache()}.
-     * The default is {@link FlushCachePolicy#DEFAULT}
-     */
-    enum FlushCachePolicy {
-        /** <code>false</code> for select statement; <code>true</code> for insert/update/delete statement. */
-        DEFAULT,
-        /** Flushes cache regardless of the statement type. */
-        TRUE,
-        /** Does not flush cache regardless of the statement type. */
-        FALSE
-    }
+  String resultSets() default "";
 }
--- src/test/java/com/andyadc/mybatis/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.andyadc.mybatis;
-
-/**
- * @author andy.an
- * @since 2018/10/25
- */
-public interface UserMapper {
-    User getUserById(Integer id);
-}
