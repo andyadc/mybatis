299903f4085e7bd1f32d17ec988d368a949f5e22
99BILL\andy.an <andy.an@99bill.com>
2019-04-11 10:56:23 +0800

    mybatis demo example

--- src/test/java/org/mybatis/example/DemoMapper.java
+++ src/test/java/org/mybatis/example/DemoMapper.java
@@ -2,19 +2,14 @@ package org.mybatis.example;
 
 import org.apache.ibatis.annotations.Mapper;
 
-import java.util.List;
-
 @Mapper
 public interface DemoMapper {
-    int deleteByPrimaryKey(Long id);
 
     int insertSelective(Demo record);
 
-    Demo selectByPrimaryKey(Long id);
+    int deleteByPrimaryKey(Long id);
 
     int updateByPrimaryKeySelective(Demo record);
 
-    List<Demo> selectByTypeList(List<Integer> types);
-
-    List<Demo> selectByTypeArray(Integer[] types);
+    Demo selectByPrimaryKey(Long id);
 }
